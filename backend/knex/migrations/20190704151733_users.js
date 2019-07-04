e115b0aa23e1bf41ddd6b2be5abb55e3fb7d2a18
verydecent <wnjhwng@gmail.com>
2019-07-04 15:24:50 -0700

    Creates Users table

---
+++ backend/knex/migrations/20190704151733_users.js
@@ -0,0 +1,33 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table
+      .increments();
+    
+    table
+      .string('username', 128)
+      .notNullable()
+      .unique();
+
+    table
+      .string('email', 128)
+      .notNullable()
+      .unique();
+    
+    table
+      .string('location', 256)
+      .notNullable();
+    
+    table
+      .timestamp('createdAt')
+      .defaultTo(knex.fn.now());
+
+    table
+      .timestamp('updatedAt')
+      .defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
