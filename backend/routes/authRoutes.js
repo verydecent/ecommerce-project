50b9964ff07bddb2316f5ba7699b5f3c1a817e30
verydecent <wnjhwng@gmail.com>
2019-07-04 17:24:09 -0700

    Implementing registration and login routes using bcryptjs

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Database = require('../models/userModel');
+
+router.post('/register', (req, res) => {
+  let body = req.body;
+  let { username, password, email, location } = body;
+  const saltRounds = 14;
+
+  if (!username || !password || !email || !location) {
+    res.status(422).json({ message: "You are missing one or more required fields" });
+  }
+  else {
+    body.password = bcrypt.hashSync(body.password, saltRounds);
+    Database.addUser(body)
+      .then(id => {
+        res.status(200).json({ message: `User ${id} was successfully added` });
+      })
+      .catch(err => {
+        res.status(500).json({ err });
+      });
+  }
+});
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(422).json({ message: "You are missing one or more required fields" });
+  }
+  else {
+    Database.getUserByEmail(email)
+      .then(user => {
+        const bodyPassword = user.password;
+
+        if (bcrypt.compareSync(password, bodyPassword)) {
+          res.status(200).json({ message: `Welcome ${user.email}`});
+        }
+        else {
+          res.status(401).json({ message: "The credentials you've entered aren't valid" });
+        }
+      })
+      .catch(err => {
+        res.status(404).json({ message: 'Non-existant email' });
+      });
+  }
+});
+
+module.exports = router;
---
+++ backend/routes/usersRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const Database = require('../models/userModel');
+
+router.get('/users', (req,res) => {
+  Database.getUsers()
+  .then(users => {
+    res.json({ users });
+  })
+  .catch(err => {
+    res.status(500).json({ error : err });
+  });
+});
