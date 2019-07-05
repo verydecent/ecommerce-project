1965e2459b1701b47c05969fdf54e7493637892e
verydecent <wnjhwng@gmail.com>
2019-07-05 08:59:13 -0700

    Boiler plating for user account related routes

---
+++ backend/routes/accountRouters.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const Database = require('../models/userModel');
+
+router.get('/listings', (req, res) => {
+
+});
+
+router.get('/favorites', (req, res) => {
+  
+});
+
+router.get('/messages', (req, res) => {
+  
+});
+
+router.get('/transactions', (req, res) => {
+  
+});
+
+router.get('/feedback', (req, res) => {
+  
+});
+
+router.get('/settings', (req, res) => {
+  
+});
+
+module.exports = router;
