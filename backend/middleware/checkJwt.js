c0d78575ea3aebf0207fadc4047cd8c8352791c6
verydecent <wnjhwng@gmail.com>
2019-07-04 23:35:39 -0700

    Typo: splot -> split

--- backend/middleware/checkJwt.js
+++ backend/middleware/checkJwt.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET || 'Monkey inside of a cage but cant see the key';
 
 function checkJwt(req, res, next) {
-  const token = req.headers['authorization'];
+  const header = req.headers['authorization'];
 
   if (typeof header !== 'undefined ') {
-    const bearer = header.splot(' ');
+    const bearer = header.split(' ');
     const token = bearer[1];
 
     jwt.verify(token, secret, (err, decodedToken) => {
