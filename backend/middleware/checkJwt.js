378a8bd641c73fb8b93e5087dc75916d546264ea
verydecent <wnjhwng@gmail.com>
2019-07-04 18:29:06 -0700

    Create checkJwt.js middleware to verify JWT

---
+++ backend/middleware/checkJwt.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET || 'Monkey inside of a cage but cant see the key';
+
+function checkJwt(req, res, next) {
+  const token = req.headers['authorization'];
+
+  if (typeof header !== 'undefined ') {
+    const bearer = header.splot(' ');
+    const token = bearer[1];
+
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Unauthorized token" });
+      }
+      else {
+        req.decodedToken = decodedToken;
+      }
+    })
+    req.token = token;
+    next();
+  }
+  else {
+    res.status(403);
+  }
+}
+
+module.exports = checkJwt;
