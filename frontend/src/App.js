f827a3b9cc256e7486eb66bf116503d78fa6025b
verydecent <wnjhwng@gmail.com>
2019-07-04 20:07:48 -0700

    Importing Login Component to Main App file and then using NavLink to connect to Login Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { NavLink, Route } from 'react-router-dom';
+
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Testing
+      <header>
+        <nav>
+          <NavLink to="/login">Login</NavLink>
+        </nav>
       </header>
+      <main>
+        {/* <Login /> */}
+        <Route path="/login" component={Login} />
+      </main>
     </div>
   );
 }
