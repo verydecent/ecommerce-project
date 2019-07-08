a7d7f82152686b489a20b091d2493e4738abde1b
verydecent <wnjhwng@gmail.com>
2019-07-07 19:00:43 -0700

    remove console logs

--- backend/routes/poolRoutes.js
+++ backend/routes/poolRoutes.js
@@ -7,8 +7,8 @@ router.get('/itemfeed', (req, res) => {
   Data('items')
     .then(items => {
       console.log(items);
-      res.status(200).json({ items });
-      // res.status(200).json(items);
+      // res.status(200).json({ items });
+      res.status(200).json(items);
     })
     .catch(err => {
       res.status(500).json({ error: 'Internal Server Error '});
