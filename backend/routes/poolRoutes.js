78e14207fe8a4b11afc2d1dec0682f2748f084e7
verydecent <wnjhwng@gmail.com>
2019-07-08 20:19:01 -0700

    must include the 'returning()' query method for Postgresql if you want it to return a specific column after inserting into DB

--- backend/routes/poolRoutes.js
+++ backend/routes/poolRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Data = require('../database/dbConfig');
 
-console.log(Data);
+// This is the pool routes, we will modularize all routes into proper router files when we stabalize a bit more
 
 router.get('/itemfeed', (req, res) => {
   Data('items')
@@ -13,6 +13,30 @@ router.get('/itemfeed', (req, res) => {
     .catch(err => {
       res.status(500).json({ error: 'Internal Server Error '});
     });
+});
+
+// Post to item to Items table
+// Authenticate to check if a user is logged in
+// Can only use this API route if the user recieves clearance through a middleware checking the JWT
+
+router.post('/account/sell', (req, res) => {
+  const itemData = req.body;
+
+  const { posted_by_user_id, price, shipping_price, title, description, category, size, color } = itemData;
+
+  if ( !posted_by_user_id || !price || !shipping_price || !title || !description || !category || !size || !color) {
+    res.status(422).json({ message: "You are missing one or more required field(s)" });
+  }
+  else {
+    Data('items').returning('posted_by_user_id').insert(itemData)
+      .then(ids => {
+        res.status(201).json({ message: `Item ${ids[0]} successfully posted` });
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Internal Server Error" });
+      })
+  }
+
 });
 
 module.exports = router;
