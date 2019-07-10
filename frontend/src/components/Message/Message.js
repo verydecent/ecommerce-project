d54748c2a7084428a682d7b8cec5db9509a7b289
verydecent <wnjhwng@gmail.com>
2019-07-10 16:54:21 -0700

    Creates Message folder

---
+++ frontend/src/components/Message/Message.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Message.css';
+
+class Message extends React.Component {
+  render() {
+    return (
+      <div className="message-container">
+        Message Component
+      </div>
+    );
+  }
+}
+
+export default Message;
