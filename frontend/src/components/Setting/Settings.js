7b79812cadd3703d5875a0efb63203011deaab31
verydecent <wnjhwng@gmail.com>
2019-07-12 13:53:22 -0700

    conditionally rendering Settings depending on props availability

--- frontend/src/components/Setting/Settings.js
+++ frontend/src/components/Setting/Settings.js
@@ -3,18 +3,25 @@ import React from 'react';
 import './Settings.css';
 
 class Settings extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state ={
-      username: '',
-      email: '',
+      newUsername: '',
+      newEmail: '',
+      newLocation: '',
+      currentPassword: '',
       newPassword: '',
       confirmNewPassword: '',
+
+      isLoading: false,
     };
   }
-  
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+  }
+
   handleSubmit =(event) => {
-    
     console.log('handleSubmit()');
     event.preventDefault();
   }
@@ -25,61 +32,81 @@ class Settings extends React.Component {
   }
 
   render() {
-    return (
+    const { username, email, location } = this.state;
+    const { user_info } = this.props;
+
+    if (user_info) return (
+      
       <div className="settings-container">
         <h1>Edit Your Info</h1>
-
         <div className="user-info-panel">
           <form className="left" onSubmit={this.handleSubmit}>
             <div className="inner">
               <label>username</label> <br />
               <input
               id="username"
-              value={this.state.username}
+              value={username}
               onChange={this.handleChange}
               type="text"
-              placeholder="this.props.username"
+              placeholder={user_info.username}
               />
             </div>
-
             <div className="inner">
               <label>email</label> <br />
               <input
               id="email"
-              value={this.state.email}
+              value={email}
               onChange={this.handleChange}
               type="text"
-              placeholder="this.props.email"
+              placeholder={user_info.email}
+              />
+            </div>
+            <div className="inner">
+              <label>location</label> <br />
+              <input
+              id="location"
+              value={location}
+              onChange={this.handleChange}
+              type="text"
+              placeholder={user_info.location}
               />
             </div>
             <button value="submit">Update</button>
           </form>
           <form className="right" onSubmit={this.handleSubmit}>
             <div className="inner">
-              <label>change password</label> <br />
+              <label>Change password</label> <br />
               <input
-              id="newPassword"
-              value={this.state.newPassword}
+              id="currentPassword"
+              value={this.state.currentPassword}
               onChange={this.handleChange}
-              placeholder="this.props.previousPassword"
+              placeholder="Current password"
               type="password"
               />
             </div>
             <div className="inner">
-              <label>confirm password</label> <br />
+              <label>New password</label> <br />
+              <input
+              id="newPassword"
+              value={this.state.newPassword}
+              onChange={this.handleChange}
+              placeholder="New password"
+              type="password"
+              />
+            </div>
+            <div className="inner">
+              <label>Confirm new password</label> <br />
               <input
               id="confirmNewPassword"
               value={this.state.confirmNewPassword}
               onChange={this.handleChange}
-              placeholder="this.props.confirmNewPassword"
+              placeholder="Confirm new password"
               type="password"
               />
             </div>
             <button value="submit">Update</button>
           </form>
         </div>
-        <div className="space">other info option update later</div>
-        <div className="space">other info option update later</div>
       </div>
     );
   }
