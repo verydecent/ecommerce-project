7c068060653a94ed0e1ec9968dbe2450b288c627
verydecent <wnjhwng@gmail.com>
2019-07-12 20:56:21 -0700

    Creating Store.js for users items

---
+++ frontend/src/components/Store/Store.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Store extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="store-container">
+
+      </div>
+    );
+  }
+}
+
+export default Store;
