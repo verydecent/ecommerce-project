e7bef8d5cb64ed4e313b7c99ace99bd625786c6d
verydecent <wnjhwng@gmail.com>
2019-07-12 22:33:33 -0700

    Making dynamic params in SubNav

--- frontend/src/components/SubNavigation/SubNavigation.js
+++ frontend/src/components/SubNavigation/SubNavigation.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 import './SubNavigation.css';
 
-function SubNavigation() {
+function SubNavigation(props) {
   return (
     <div className="account-sub-nav">
       <div className="account-sub-nav-header">
@@ -15,7 +15,7 @@ function SubNavigation() {
       <div className="account-sub-nav-content">
         <NavLink className="nav-link" to="/account/settings">Settings</NavLink>
         <NavLink className="nav-link" to="/account/messages">Messages</NavLink>
-        <NavLink className="nav-link" to="/account/store">My Store</NavLink>
+        <NavLink className="nav-link" to={`/account/store/${props.user_id}`}>My Store</NavLink>
         <NavLink className="nav-link" to="/account/favorites">Favorites</NavLink>
         <NavLink className="nav-link" to="/account/feedback">Feedback</NavLink>
         <NavLink className="nav-link" to="/account/transactions">Transactions</NavLink>
