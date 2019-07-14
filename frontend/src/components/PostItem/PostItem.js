c5c3349e9cce22480b44e761d7344e895adf37c6
verydecent <wnjhwng@gmail.com>
2019-07-13 19:19:34 -0700

    Wrap inputs in form and creates a button and temporary handleSubmit event handler

--- frontend/src/components/PostItem/PostItem.js
+++ frontend/src/components/PostItem/PostItem.js
@@ -16,13 +16,15 @@ class PostItem extends React.Component {
     };
   }
 
+  handleSubmit = (event) => {
+    console.log('handleSubmit')
+  }
+
   handleChange = (event) => {
     const { id, value } = event.target;
     this.setState({ [id]: value });
     event.preventDefault();
   }
-
-
 
   render() {
     const { price, shipping_price, title, description, category, size, color } = this.state;
@@ -30,90 +32,91 @@ class PostItem extends React.Component {
       <div className="post-item-container">
         <h1>Post Item</h1>
         <div className="item-panel">
-
-
-          <div className="item-detail-panel">
-            <h3>Details</h3>
-            <div className="left-column">
-              <div className="">
-                <input 
-                  id="title"
-                  value={title}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="Title"
-                />
+          <form onSubmit={this.handleSubmit}>
+            <div className="item-detail-panel">
+              <h3>Details</h3>
+              <div className="left-column">
+                <div className="">
+                  <input 
+                    id="title"
+                    value={title}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Title"
+                  />
+                </div>
+                <div className="">
+                  <input 
+                    id="category"
+                    value={category}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Category"
+                  />
+                </div>
+                <div className="">
+                  <input 
+                    id="size"
+                    value={size}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Size"
+                  />
+                </div>
+                <div className="">
+                  <input 
+                    id="color"
+                    value={color}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Color"
+                  />
+                </div>
               </div>
-              <div className="">
-                <input 
-                  id="category"
-                  value={category}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="Category"
-                />
-              </div>
-              <div className="">
-                <input 
-                  id="size"
-                  value={size}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="Size"
-                />
-              </div>
-              <div className="">
-                <input 
-                  id="color"
-                  value={color}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="Color"
-                />
+              <div className="right-column">
+                <div className=""></div>
+                <div className=""></div>
               </div>
             </div>
-            <div className="right-column">
-              <div className=""></div>
-              <div className=""></div>
-            </div>
-          </div>
 
-          <div className="item-description-panel">
-            <h3>Description</h3>
-            <textarea
-              id="description"
-              value={description}
-              onChange={this.handleChange}
-              type="text"
-              placeholder="Retail Price, Condition, Measurements, Shipping Policy, Link to Retail Page, etc"
-            />
-          </div>
-          <div className="">
-            <h3>Payment</h3>
-            <div className="">
-              <input 
-                id="price"
-                value={price}
+            <div className="item-description-panel">
+              <h3>Description</h3>
+              <textarea
+                id="description"
+                value={description}
                 onChange={this.handleChange}
                 type="text"
-                placeholder="$0.00"
+                placeholder="Retail Price, Condition, Measurements, Shipping Policy, Link to Retail Page, etc"
               />
             </div>
             <div className="">
-              <input 
-                id="shipping_price"
-                value={shipping_price}
-                onChange={this.handleChange}
-                type="text"
-                placeholder="$0.00"
-              />
+              <h3>Payment</h3>
+              <div className="">
+                <input 
+                  id="price"
+                  value={price}
+                  onChange={this.handleChange}
+                  type="text"
+                  placeholder="$0.00"
+                />
+              </div>
+              <div className="">
+                <input 
+                  id="shipping_price"
+                  value={shipping_price}
+                  onChange={this.handleChange}
+                  type="text"
+                  placeholder="$0.00"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="item-image-panel">
-            <h3>Photos</h3>
-            <img src="https://www.sunspel.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/m/t/mtsh0001-whaa-1new.jpg" alt="white t shirt" />
-          </div>
+            <div className="item-image-panel">
+              <h3>Photos</h3>
+              <img src="https://www.sunspel.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/m/t/mtsh0001-whaa-1new.jpg" alt="white t shirt" />
+            </div>
+            <button value="submit">Post Item</button>
+          </form>
         </div>
       </div>
     );
