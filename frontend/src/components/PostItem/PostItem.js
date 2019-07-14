ed48cc9e27c393f1020a15ced176b711c05700b0
verydecent <wnjhwng@gmail.com>
2019-07-13 20:31:13 -0700

    minimal styling, and got forms running still need to work on axios post and then figuring out how to upload photos

--- frontend/src/components/PostItem/PostItem.js
+++ frontend/src/components/PostItem/PostItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './PostItem.css';
 
+const tempIMG ="https://www.sunspel.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/m/t/mtsh0001-whaa-1new.jpg";
+
 class PostItem extends React.Component {
   constructor() {
     super();
@@ -18,6 +20,10 @@ class PostItem extends React.Component {
 
   handleSubmit = (event) => {
     console.log('handleSubmit')
+    console.log('this.props.match',this.props.match)
+    console.log(this.state.posted_by_user_id);
+
+    event.preventDefault();
   }
 
   handleChange = (event) => {
@@ -35,47 +41,47 @@ class PostItem extends React.Component {
           <form onSubmit={this.handleSubmit}>
             <div className="item-detail-panel">
               <h3>Details</h3>
-              <div className="left-column">
-                <div className="">
-                  <input 
-                    id="title"
-                    value={title}
-                    onChange={this.handleChange}
-                    type="text"
-                    placeholder="Title"
-                  />
+              <div className="item-details">
+                <div className="left-column">
+                  <div className="">
+                    <input 
+                      id="title"
+                      value={title}
+                      onChange={this.handleChange}
+                      type="text"
+                      placeholder="Title"
+                    />
+                  </div>
+                  <div className="">
+                    <input 
+                      id="category"
+                      value={category}
+                      onChange={this.handleChange}
+                      type="text"
+                      placeholder="Category"
+                    />
+                  </div>
                 </div>
-                <div className="">
-                  <input 
-                    id="category"
-                    value={category}
-                    onChange={this.handleChange}
-                    type="text"
-                    placeholder="Category"
-                  />
+                <div className="right-column">
+                  <div className="">
+                    <input 
+                      id="size"
+                      value={size}
+                      onChange={this.handleChange}
+                      type="text"
+                      placeholder="Size"
+                    />
+                  </div>
+                  <div className="">
+                    <input 
+                      id="color"
+                      value={color}
+                      onChange={this.handleChange}
+                      type="text"
+                      placeholder="Color"
+                    />
+                  </div>
                 </div>
-                <div className="">
-                  <input 
-                    id="size"
-                    value={size}
-                    onChange={this.handleChange}
-                    type="text"
-                    placeholder="Size"
-                  />
-                </div>
-                <div className="">
-                  <input 
-                    id="color"
-                    value={color}
-                    onChange={this.handleChange}
-                    type="text"
-                    placeholder="Color"
-                  />
-                </div>
-              </div>
-              <div className="right-column">
-                <div className=""></div>
-                <div className=""></div>
               </div>
             </div>
 
@@ -89,33 +95,44 @@ class PostItem extends React.Component {
                 placeholder="Retail Price, Condition, Measurements, Shipping Policy, Link to Retail Page, etc"
               />
             </div>
-            <div className="">
+            <div className="item-payment-panel">
               <h3>Payment</h3>
-              <div className="">
-                <input 
-                  id="price"
-                  value={price}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="$0.00"
-                />
-              </div>
-              <div className="">
-                <input 
-                  id="shipping_price"
-                  value={shipping_price}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="$0.00"
-                />
+              <div className="payment-details">
+                <div className="left-column">
+                  <input 
+                    id="price"
+                    value={price}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Item price"
+                  />
+                </div>
+                <div className="right-column">
+                  <input 
+                    id="shipping_price"
+                    value={shipping_price}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Shipping fees"
+                  />
+                </div>
               </div>
             </div>
 
             <div className="item-image-panel">
               <h3>Photos</h3>
-              <img src="https://www.sunspel.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/m/t/mtsh0001-whaa-1new.jpg" alt="white t shirt" />
+              <div className="item-images">
+                <div className="header-image">
+                  <img src={tempIMG} alt="white t shirt" />
+                </div>
+
+                <button>Upload Photos</button>
+              </div>
             </div>
-            <button value="submit">Post Item</button>
+
+            <div className="item-form-button">
+              <button value="submit">Post Item</button>
+            </div>
           </form>
         </div>
       </div>
