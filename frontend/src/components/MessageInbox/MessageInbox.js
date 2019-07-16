1a7c8c8cf673e7249bc75e7c727fd3393544638a
verydecent <wnjhwng@gmail.com>
2019-07-15 18:01:15 -0700

    Rename Message to MessageInbox

---
+++ frontend/src/components/MessageInbox/MessageInbox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './MessageInbox.css';
+
+class MessageInbox extends React.Component {
+  render() {
+    return (
+      <div className="message-inbox-container">
+        <h1>Message Inbox</h1>
+      </div>
+    );
+  }
+}
+
+export default MessageInbox;
