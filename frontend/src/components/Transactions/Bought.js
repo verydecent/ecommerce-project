8839f0b7634351541178af2d9befd57807050dfa
verydecent <wnjhwng@gmail.com>
2019-07-19 16:25:43 -0700

    Convert to class component

--- frontend/src/components/Transactions/Bought.js
+++ frontend/src/components/Transactions/Bought.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function Bought() {
-  return (
-    <div>
-      bought list
-    </div>
-  );
+class Bought extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Bought list
+      </div>
+    );
+  }
 }
 
 export default Bought;
