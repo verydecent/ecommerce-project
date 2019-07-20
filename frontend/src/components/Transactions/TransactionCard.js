4ed3186a3143fe187b7bf6a97ba84be1daefa2e9
verydecent <wnjhwng@gmail.com>
2019-07-20 16:15:57 -0700

    Changing to class to implement Modal

--- frontend/src/components/Transactions/TransactionCard.js
+++ frontend/src/components/Transactions/TransactionCard.js
@@ -4,33 +4,47 @@ import { formatTransactionDate } from '../../Helpers/transactionDate';
 
 const testIMG ="https://www.sunspel.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/m/t/mtsh0001-whaa-1new.jpg";
 
-function TransactionCard(props) {
-  const { id, title, price, updated_at } = props.item;
-  return (
-    <div className="transaction-card-container">
-      <Link to={`/item/${id}`}>
-        <div className="transaction-card-img">
-          <img src={testIMG} alt=""/>
-        </div>
-      </Link>
+class TransactionCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+  }
 
-      <div className="transaction-card-details">
-
-        <div className="transaction-card-details-left">
-          <Link to={`/item/${id}`}>
-            <h1>{title}</h1>
-          </Link>
-          <h1>Price {price}</h1>
-          <h1>{formatTransactionDate(updated_at)}</h1>
-          
-        </div>
-
-        <div className="transaction-card-details-right">
-          <button>Leave Feedback</button>
+  render() {
+    const { id, title, price, updated_at } = this.props.item;
+    return (
+      <div className="transaction-card-container">
+        <Link to={`/item/${id}`}>
+          <div className="transaction-card-img">
+            <img src={testIMG} alt=""/>
+          </div>
+        </Link>
+  
+        <div className="transaction-card-details">
+  
+          <div className="transaction-card-details-left">
+            <Link to={`/item/${id}`}>
+              <h1>{title}</h1>
+            </Link>
+            <h1>Price ${price}</h1>
+            <h1>{formatTransactionDate(updated_at)}</h1>
+            
+          </div>
+  
+          <div className="transaction-card-details-right">
+            <button onClick={this.showModal}>
+              Leave Feedback
+            </button>
+            {/* Open Modal */}
+  
+            {/* Inside modal will have the post request passing it the item details*/}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default TransactionCard;
