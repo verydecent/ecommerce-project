ca5255e3d4241c8910d4af14f357b36413e6a182
verydecent <wnjhwng@gmail.com>
2019-07-20 15:50:03 -0700

    Remove old seeds, and recreate new items seed with Brand included

---
+++ backend/database/seeds/001_items.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('items').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('items').insert([
+        {id: 1, posted_by_user_id: 1, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+        {id: 2, posted_by_user_id: 2, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+        {id: 3, posted_by_user_id: 3, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+        {id: 4, posted_by_user_id: 1, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+        {id: 5, posted_by_user_id: 2, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+        {id: 6, posted_by_user_id: 3, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+        {id: 7, posted_by_user_id: 1, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+        {id: 8, posted_by_user_id: 2, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+        {id: 9, posted_by_user_id: 3, price: 100, shipping_price: 10, brand: 'amex black', title: 'test 1', description: 'test-description', category: 'clothing', size: 'Medium', color: 'Black'},
+      ]);
+    });
+};
