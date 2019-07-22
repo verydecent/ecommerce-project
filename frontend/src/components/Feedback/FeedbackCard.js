c97a729d5bb56ed45845d6ab07e604711007530b
verydecent <wnjhwng@gmail.com>
2019-07-22 16:03:03 -0700

    Finished styling feedback card

--- frontend/src/components/Feedback/FeedbackCard.js
+++ frontend/src/components/Feedback/FeedbackCard.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './FeedbackCard.css';
 
+const testIMG ="https://www.sunspel.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/m/t/mtsh0001-whaa-1new.jpg";
+
 function FeedbackCard() {
   return (
     <div className="feedback-card-container">
       <div className="feedback-panel-left">
-        Left
+        <div className="feedback-item-date">
+          HEADER DATE 
+        </div>
+        <div className="feedback-item-info">
+          {/* <Link></Link> */}
+          <div className="feedback-item-title">ITEM NAME</div>
+          <div className="feedback-item-description">FEEDBACK DESC</div>
+        </div>
       </div>
 
       <div className="feedback-panel-right">
-        Right
+        {/* <Link></Link> */}
+        <img src={testIMG} alt="9999" />
       </div>
 
     </div>
