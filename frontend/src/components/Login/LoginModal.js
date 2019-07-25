ce2c227ad013baa850d106494a066f463fdaf609
verydecent <wnjhwng@gmail.com>
2019-07-25 01:19:26 -0700

    Old Login components deleted and updated with new Login Modal :D

--- frontend/src/components/Login/LoginModal.js
+++ frontend/src/components/Login/LoginModal.js
@@ -1,19 +1,85 @@
 import React from 'react';
+import axios from 'axios';
+import { loginUser } from '../../Helpers/devEndpoints';
+import { withRouter } from 'react-router-dom';
+
+import './LoginModal.css';
 
 class LoginModal extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      show: false,
+      email: '',
+      password: '',
     };
   }
+
+  handleSubmit = (event) => {
+    const { verifyUser, toggleLoginModal, history } = this.props;
+    const body = { email: this.state.email, password: this.state.password };
+    axios
+      .post(loginUser(), body)
+      .then(response => {
+        localStorage.setItem('jwt', response.data['token']);
+        this.setState({ email: '', password: '' });
+        verifyUser();
+        toggleLoginModal();
+        history.push('/');
+      })
+      .catch(error => {
+        console.error('ERROR', error);
+      });
+    event.preventDefault();
+  }
+
+  handleChange = (event) => {
+    const { id, value } = event.target;
+    this.setState({ [id]: value});
+  }
+
   render() {
+    const { email, password } = this.state;
+    const { toggleLoginModal } = this.props;
     return (
       <div className="login-modal-container">
-        Modal Login :D
+        <div className="login-content">
+          <div className="login-modal-header">
+            <h1>Login</h1>
+            <div className="exit-modal" onClick={toggleLoginModal}>x</div>
+          </div>
+          <form className="login-form" onSubmit={this.handleSubmit}>
+            <div className="login-field">
+              <label className="--label">Email</label>
+              <input
+                className="--input"
+                id="email"
+                onChange={this.handleChange}
+                value={email}
+                type="text"
+                placeholder="Email Address"
+              />
+            </div>
+            <div className="login-field">
+              <label className="--label">Password</label>
+              <input
+                className="--input"
+                id="password"
+                onChange={this.handleChange}
+                value={password}
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <p className="forgot-password">Forgot Password?</p>
+            <div className="login-button">
+              <button value="submit">Login</button>
+            </div>
+          </form>
+          <div className="login-content-spacer"></div>
+        </div>
       </div>
     );
   }
 }
 
-export default LoginModal;
+export default withRouter(LoginModal);
