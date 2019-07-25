aa71bf74f12b7c9fc41987eee2bd3443d57bb1fe
verydecent <wnjhwng@gmail.com>
2019-07-25 03:02:55 -0700

    Dropdown menu

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-
-const x = {
-  width: '100%',
-  border: '1px solid green'
-}
+import { NavLink } from 'react-router-dom';
+import logo from '../../Images/logo.png';
+import './Navigation.css';
 
 class Navigation extends React.Component {
   constructor() {
@@ -15,20 +12,71 @@ class Navigation extends React.Component {
   }
 
   render() {
-    return (
-      <div className="Navigation-div" style={x}>
-        <nav className="navbar">
-          <div className="container">
-            Nav - Navigation
+    const jwt = localStorage.getItem('jwt');
+    const { user_id, toggleLoginModal, toggleRegisterModal } = this.props;
+    if (user_id && jwt) {
+      return (
+        <div className="global-nav-container">
+          <div className="global-nav">
+            <div className="logo-container">
+              <NavLink to="/">
+                <img className="logo" src={logo} alt="" />
+              </NavLink>
+            </div>
             <nav>
-              <NavLink to="">Home</NavLink>
-              <NavLink to="">Sell</NavLink>
-              <NavLink to="">Favorites</NavLink>
-              <NavLink to="">Feedback</NavLink>
-              <NavLink to="">Account</NavLink>
+              <ul className="nav__links">
+                <li><NavLink className="nav__link" to="/">Home</NavLink></li>
+                <li onClick={this.toggleLoginModal}>Login</li>
+                <li onClick={this.toggleRegisterModal}>Register</li>
+                <li><NavLink className="nav__link" to="/account/settings">Account</NavLink></li>
+                <li onClick={this.handleLogout}>Log Out</li>
+              </ul>
             </nav>
+
+            <div className="dropdown">
+              <button className="global-nav-button">Account</button>
+              <div className="dropdown-content">
+                <div className="dropdown-item">Post Item</div>
+                <div className="dropdown-item">Settings</div>
+                <div className="dropdown-item">Messages</div>
+                <div className="dropdown-item">My Store</div>
+                <div className="dropdown-item">Favorites</div>
+                <div className="dropdown-item">Transactions</div>
+              </div>
+            </div>
           </div>
-        </nav>
+          <div className="spacer"></div>
+        </div>
+      );
+    }
+    
+    return (
+      <div className="global-nav-container">
+        <div className="global-nav">
+          <div className="logo-container">
+            <NavLink to="/">
+              <img className="logo" src={logo} alt="" />
+            </NavLink>
+          </div>
+          <nav>
+            <ul className="nav__links">
+              <li><NavLink className="nav__link" to="/">Home</NavLink></li>
+              <li onClick={this.toggleLoginModal}>Login</li>
+              <li onClick={this.toggleRegisterModal}>Register</li>
+              <li><NavLink className="nav__link" to="/account/settings">Account</NavLink></li>
+              <li onClick={this.handleLogout}>Log Out</li>
+            </ul>
+          </nav>
+
+          <div className="dropdown">
+            <button className="global-nav-button">Account</button>
+            <div className="dropdown-content">
+              <div className="dropdown-item" onClick={toggleLoginModal}>Login</div>
+              <div className="dropdown-item" onClick={toggleRegisterModal}>Register</div>
+            </div>
+          </div>
+        </div>
+        <div className="spacer"></div>
       </div>
     );
   }
