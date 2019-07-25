080742a312dc4aad016bb2f6177a1670d454ff77
verydecent <wnjhwng@gmail.com>
2019-07-24 21:37:26 -0700

    New Login Modal component

---
+++ frontend/src/components/Login/LoginModal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginModal() {
+  return (
+    <div className="login-modal-container">
+      Modal Login :D
+    </div>
+  );
+}
+
+export default LoginModal;
