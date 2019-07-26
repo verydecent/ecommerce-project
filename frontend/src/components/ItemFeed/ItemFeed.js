0ee1d3932ca85e2706d1647dedb0e8e539daa8b7
verydecent <wnjhwng@gmail.com>
2019-07-25 20:18:37 -0700

    Creates withCat HOC to conditionally rendering items, based on NavLink Param or by Category

--- frontend/src/components/ItemFeed/ItemFeed.js
+++ frontend/src/components/ItemFeed/ItemFeed.js
@@ -1,41 +1,21 @@
 import React from 'react';
-import axios from 'axios';
-
-import { getItems } from '../../Helpers/devEndpoints';
-
-import './ItemFeed.css';
+import withCategories from '../../Helpers/withCategories';
 import ItemDisplay from '../ItemDisplay/ItemDisplay';
+import './ItemFeed.css';
 
 class ItemFeed extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      items: []
-    }
-  } 
-  
-
-  componentDidMount() {
-    axios.get(getItems())
-      .then(({ data }) => {
-        this.setState({ items: data });
-      })
-      .catch(err => {
-        console.error( `Error ${err} `);
-      });
-  }
-
   render() {
-    const { items } = this.state;
+    console.log('props', this.props);
+    const { items, title } = this.props;
     const { liked, handleLike } = this.props;
-    const filtered = items.filter((item, index) => (item.is_available == 1));
-    const available = filtered.map((item, index) => (
+    // const filtered = items.filter((item, index) => (item.is_available == 1));
+    const available = items.map((item, index) => (
       <ItemDisplay key={index} item={item} liked={liked} handleLike={handleLike} />
     ));
     return (
       <div className="item-feed-container">
-        <div className="tag-line">
-          <span>Browse The Feed</span>
+        <div className="feed-header">
+          <span>{title}</span>
         </div>
         <div className="item-feed-panel">
           {available}
@@ -45,4 +25,4 @@ class ItemFeed extends React.Component {
   }
 }
 
-export default ItemFeed;
+export default withCategories(ItemFeed);
