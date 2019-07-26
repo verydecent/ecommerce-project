9c2abc236c1a8985a4f75f9f090d3fba48900d1e
verydecent <wnjhwng@gmail.com>
2019-07-26 01:24:30 -0700

    User Store accessing data properly, now time to style

--- frontend/src/components/User/UserStore.js
+++ frontend/src/components/User/UserStore.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import './UserStore.css;';
+import axios from 'axios';
+import ItemDisplay from '../ItemDisplay/ItemDisplay';
+import { getStoreByUsername } from '../../Helpers/devEndpoints';
+import './UserStore.css';
 
 class UserStore extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_info: {},
+      user_items: [],
+    };
+  }
 
+  componentDidMount() {
+    const { username } = this.props.match.params;
+    axios.get(getStoreByUsername(username))
+      .then(response => {
+        const { user_info, user_items } = response.data;
+        this.setState({ user_info, user_items });
+      })
+      .catch(error => console.error(error));
+  }
 
   render() {
+    const { user_info, user_items } = this.state;
+    const { liked, handleLike } = this.props;
+    let items = user_items.map((item, index) => (<ItemDisplay index={index} item={item} liked={liked} handleLike={handleLike} />));
+
     return (
       <div className="user-store-container">
         <div className="user-store-panel">
-
+          <div className="user-header-card">
+            <div className="header-card-img">
+              <img src="" alt="" />
+            </div>
+            <div className="header-card-details">
+              <p>USERNAME</p>
+              <p>LOCATION</p>
+            </div>
+          </div>
+          <div className="store-item-feed">
+            {items}
+          </div>
         </div>
       </div>
     );
