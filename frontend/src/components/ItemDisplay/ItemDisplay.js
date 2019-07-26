2f100ae1acd4998b3220ab8e8aad1c4baceaf51e
verydecent <wnjhwng@gmail.com>
2019-07-25 20:04:13 -0700

    Introduce dynamic branding

--- frontend/src/components/ItemDisplay/ItemDisplay.js
+++ frontend/src/components/ItemDisplay/ItemDisplay.js
@@ -7,7 +7,7 @@ import './ItemDisplay.css';
 const testIMG ="https://www.sunspel.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/m/t/mtsh0001-whaa-1new.jpg";
 
 function ItemDisplay(props) {
-  const { id, price, title, size, created_at } = props.item;
+  const { id, brand, price, title, size, created_at } = props.item;
   const { handleLike, liked } = props;
 
   return (
@@ -25,7 +25,7 @@ function ItemDisplay(props) {
         </h3>
         <div className="item-metadata">
           <div className="item-brand-and-size">
-            <h3 className="item-brand">Raf Simons</h3>
+            <h3 className="item-brand">{brand}</h3>
             <h3 className="item-size">{size}</h3>
           </div>
 
