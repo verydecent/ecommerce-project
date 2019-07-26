b6b4b27df07fd715fd9d23b9b7d676e459ae5e98
verydecent <wnjhwng@gmail.com>
2019-07-25 19:23:33 -0700

    Initial set up for categories nav, set up paths

--- frontend/src/components/CategoriesNav/CategoriesNav.js
+++ frontend/src/components/CategoriesNav/CategoriesNav.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './CategoriesNav.css';
 
 function CategoriesNav() {
   return (
     <div className="categories-nav">
-      <div>Designers</div>
-      <div>Browser By Category</div>
-      <div>Sneakers</div>
-      <div>Footwear</div>
-      <div>Tops</div>
-      <div>Outerwear</div>
-      <div>Staff Picks</div>
-      <div>Collections</div>
+      <NavLink className="cat__nav__link" to="/">Brands</NavLink>
+      <NavLink className="cat__nav__link" to="/item-feed/accesories">Accessories</NavLink>
+      <NavLink className="cat__nav__link" to="/item-feed/outerwear">Outerwear</NavLink>
+      <NavLink className="cat__nav__link" to="/item-feed/tops">Tops</NavLink>
+      <NavLink className="cat__nav__link" to="/item-feed/bottoms">Bottoms</NavLink>
+      <NavLink className="cat__nav__link" to="/item-feed/footwear">Footwear</NavLink>
+      <NavLink className="cat__nav__link" to="/item-feed/deals">Deals Under $200</NavLink>
     </div>
   );
 }
