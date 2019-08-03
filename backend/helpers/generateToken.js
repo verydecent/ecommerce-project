bc9abeb7db58bfa08775ad33fdbe30947abe7c24
verydecent <wnjhwng@gmail.com>
2019-08-03 15:53:26 -0700

    Removed images from generate token helper

--- backend/helpers/generateToken.js
+++ backend/helpers/generateToken.js
@@ -2,14 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const secret = process.env.JWT_SECRET || 'Monkey inside of a cage but cant see the key';
 
-function generateToken({ user, image }) { 
+function generateToken({ user }) { 
 
   const payload = {
     id: user.id,
     username: user.username,
     location: user.location,
     email: user.email,
-    user_profile: image.url,
     roles: ['admin']
   }
 
